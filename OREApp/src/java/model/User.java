fb0ef32a3846e8f369120d42d0c5a671dd7a8a5a
Alexander Andreevič Osiik <ao.osiik@googlemail.com>
2020-04-04 14:18:25 +0200

    Create User.java

---
+++ OREApp/src/java/model/User.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Alexander
+ */
+public class User implements Serializable {
+
+    private String email;
+    private String password;
+    
+    private String firstName;
+    private String lastName;
+//    // TODO: how to best serialize them?
+//    private Address address;
+//    private List<Recipe> recipes;
+//    private List<Recipe> favorites;
+    
+    public User(){}
+    
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    
+}
