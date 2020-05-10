e751c71fcc448192d61462b09947b0c95ab28fb1
Alexander Andreevič Osiik <ao.osiik@googlemail.com>
2020-05-11 00:04:33 +0200

    Comment Class
    Class Representing a user comment

---
+++ OREApp/src/java/model/Comment.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Alexander
+ */
+public class Comment implements Serializable {
+    
+    int userId;
+    int recipeId;
+    String comment;
+
+    public Comment(){}
+    
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getRecipeId() {
+        return recipeId;
+    }
+
+    public void setRecipeId(int recipeId) {
+        this.recipeId = recipeId;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+}
