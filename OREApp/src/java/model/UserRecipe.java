dca65e58a2e310320bfbbb9086df5e859d677d60
Alexander Andreevič Osiik <ao.osiik@googlemail.com>
2020-05-11 00:04:11 +0200

    Create UserRecipe.java
    UserRecipe extends Recipe by username, rating and comments

---
+++ OREApp/src/java/model/UserRecipe.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ *
+ * @author Alexander
+ */
+public class UserRecipe extends Recipe implements Serializable        
+{
+    String username;
+    double rating;
+    List<Comment> comments; 
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    public List<Comment> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<Comment> comments) {
+        this.comments = comments;
+    }
+    
+}
